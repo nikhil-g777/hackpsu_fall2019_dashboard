f3e1f7d7ac7671b9bc76046f3ae7282c14dd7fa2
nikhilg777 <nikhil.g777@gmail.com>
2019-11-03 13:36:00 -0500

    first

--- src/components/tickets/Incidents.js
+++ src/components/tickets/Incidents.js
@@ -5,8 +5,11 @@ const Incidents = () => {
     return (
         <div>
             <div className="alert alert-primary" role="alert">
-                A simple primary alert—check it out!
-            </div>        
+                Heavy storm in SaltLake City, Utah - reported by Alex at 11/03/2019 11:00 AM
+            </div>
+            <div className="alert alert-primary" role="alert">
+                Fire receeding in Austin, Texas - source: twitter at 11/01/2019 05:00 PM            
+            </div>
         </div>
     );
 };
